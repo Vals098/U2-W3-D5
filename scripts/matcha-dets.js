bedeb6d2aaa90535fb851275e925c18cb4983fc5
Vals098 <valeria.farinosi@gmail.com>
2026-04-17 10:19:15 +0200

    add: setup and starting point

---
+++ scripts/matcha-dets.js
@@ -0,0 +1,5 @@
+fetch("https://striveschool-api.herokuapp.com/api/product/", {
+headers: {
+"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2OWUxZGQ2NjczOWY4NzAwMTU3YWIwODMiLCJpYXQiOjE3NzY0MDk5NTgsImV4cCI6MTc3NzYxOTU1OH0.-qJeuO-VKE8DlT11R-vB-C0jcfUgvSC2JsNHNeDvF2A"
+}
+})
